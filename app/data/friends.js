29daa3be45668a8cbce6226d36a331f430a04859
vcore83 <Saggy@MacBook-Pro.local>
2017-05-09 16:05:43 -0400

    push-problem

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+
+  {
+    name: "Vito Corleone",
+    photo: "#",
+    scores: [
+      '5',
+      '4',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3'
+    ]
+  },
+  {
+    name: "Sonny Corleone",
+    photo: "#",
+    scores: [
+      '5',
+      '4',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3'
+    ]
+  },
+  {
+    name: "Michael Corleone",
+    photo: "#",
+    scores: [
+      '5',
+      '4',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3',
+      '3'
+    ]
+  }
+]
+
+module.exports = friends;
